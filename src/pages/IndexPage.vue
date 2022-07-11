914fc0e26ec6f9f54e446b3247301aca5c44ab22
Aleksandra <aleksandra.shabanova@ut.ee>
2022-07-11 16:29:45 +0500

    Draft of OrderPage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,28 @@
 <template>
   <q-page> <!-- class="flex flex-center" -->
-    <div class="row full-width text-h6 text-center">
+    <q-footer bordered class="bg-white text-primary">
+      <div class= "row full-width">
+        <div class="col-1">
+          <q-btn flat style="color: #FF0080" label="Отменить"/>
+        </div>
+        <div class="col-10"></div>
+        <div class="col-1">
+          <q-btn flat style="color: $blue" label="Создать"/>
+        </div>
+      </div>
+    </q-footer>
+
+    <div class="row full-width justify-end">
+      Автор документа: Александра
+    </div>
+
+    <div class="row full-screen text-h6 justify-center q-pt-xl q-pr-xl">
       Заказ № 00001 от 08 Июль 2022
     </div>
-    <div class="fill">
+    <div class="fill q-pt-xl">
+      <q-select outlined v-model="model" :options="options" label="Контрагент" />
+    </div>
+    <div class="fill q-pt-xl">
       <q-table
         style="height: 400px"
         title="Материалы"
@@ -16,10 +35,6 @@
       >
         <template v-slot:body-cell="props">
           <q-td :props="props">
-            <!-- props.row.name -->
-            <!-- props.value -->
-            {{ props.col.field }}<br/>
-            {{ _get(props.row, props.col.field) }}
             <q-field>
               <template v-slot:control>
                 <selector :value="props.row" :field="props.col.field" :label="props.col.label"/>
@@ -28,6 +43,32 @@
           </q-td>
         </template>
       </q-table>
+    </div>
+
+    <div class="fill q-pt-xl">
+      <q-table
+        style="height: 400px"
+        title="Работы"
+        :rows="rows"
+        :columns="columns"
+        row-key="index"
+        virtual-scroll
+        v-model:pagination="pagination"
+        :rows-per-page-options="[0]"
+      >
+        <template v-slot:body-cell="props">
+          <q-td :props="props">
+            <q-field>
+              <template v-slot:control>
+                <selector :value="props.row" :field="props.col.field" :label="props.col.label"/>
+              </template>
+            </q-field>
+          </q-td>
+        </template>
+      </q-table>
+    </div>
+    <div class="row full-width text-h6 text-center q-pt-xl q-pr-xl">
+        Итоги
     </div>
   </q-page>
 </template>
@@ -93,5 +134,11 @@ const _get = (obj, path) => {
   return current;
 }
 const _set = set
+
+const model = ref(null);
+
+const options = [
+  'Agent 1', 'Agent 2', 'Agent 3', 'Agent 4', 'Agent 5'
+]
 
 </script>
