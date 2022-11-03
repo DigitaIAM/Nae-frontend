8d82f180841815b21b2aa7fa355c7ebe8a7a0a3b
Dmitriy Shabanov <shabanovd@gmail.com>
2022-11-03 15:22:24 +0500

    prototype

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,11 +1,8 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:3030' })
-
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios
-  app.config.globalProperties.$api = api
 })
 
-export { axios, api }
+export { axios }
