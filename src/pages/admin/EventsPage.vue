bb38f7e0e8a63f71f7bf8f7a83699cf223820e0c
Dmitriy Shabanov <shabanovd@gmail.com>
2022-11-11 20:50:07 +0500

    prototype

--- src/pages/admin/EventsPage.vue
+++ src/pages/admin/EventsPage.vue
@@ -10,6 +10,7 @@
           { name: 'employeeNoString', label: 'employeeNoString', field: row => label(row.event, 'employeeNoString'), required: true, align: 'center', sortable: false},
           { name: 'name', label: 'name', field: row => label(row.event, 'name'), required: true, align: 'center', sortable: false},
         ]"
+        :context="{ oid: orgId }"
         :fields="[
           // { name: 'time', type: 'text', field: 'time' },
           // { name: 'employeeNoString', type: 'text', field: 'employeeNoString' },
@@ -23,7 +24,11 @@
 <script setup>
 import JornalAndForm from '../../components/JornalAndForm.vue';
 
+import { storeToRefs } from 'pinia'
+import { useOid } from '../../stores/oid'
 import { useEvents } from '../../stores/events'
+
+const { orgId } = storeToRefs(useOid())
 const events = useEvents()
 
 const label = (row, name) => {
